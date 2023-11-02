e147823e184dd3e34314965e45d3ad7c2bcc7918
Thiago Masaharu Sakugawa <thi.thiagosakugawa@gmail.com>
2023-11-02 19:54:35 -0300

    api de buscar paciente por nome

--- main.py
+++ main.py
@@ -20,6 +20,40 @@ def get_paciente():
     finally:
         cursor.close() 
         conn.close()
+
+@app.route('/get_paciente_nome', methods=['POST'])
+def buscar_nome():
+    try:        
+        name = request.json['name']
+
+        if request.method == 'POST':
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)		
+            cursor.execute(f"""SELECT * 
+                               FROM paciente
+                               WHERE nome LIKE '%{name}%'
+                            """)
+            paciente_registros = cursor.fetchall()
+            response = jsonify(paciente_registros)
+            response.status_code = 200
+            return response
+        else:
+            return showMessage()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close() 
+
+@app.errorhandler(404)
+def showMessage(error=None):
+    message = {
+        'status': 404,
+        'message': 'Record not found: ' + request.url,
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
         
 if __name__ == "__main__":
     app.run()
